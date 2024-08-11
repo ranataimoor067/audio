d7475ca5c01f9b4d5682da6964a3867275308438
ranataimoor067 <149140170+ranataimoor067@users.noreply.github.com>
2024-08-11 15:15:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var audio = new Audio('PTT-20240805-WA0006.opus');
+var audio = new Audio('AUD-20240806-WA0003.mp3');
 audio.addEventListener('canplaythrough', function() {
 audio.loop = true;
 audio.play();
